a835a4b4e136c9ead67468dd7d102c2b30221771
Abdullah Aziz Sharfuddin Shakkhor <abdullahshakkhor@gmail.com>
2025-02-26 17:49:20 +0600

    all patterns done

---
+++ pattern22.cpp
@@ -0,0 +1,41 @@
+//Pattern - 22: The Number Pattern
+
+#include<stdio.h>
+#include <string>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void hollowRectangle(int n) 
+{
+    int k  = 1;
+
+    for(int i = 1; i<n*2; i++) 
+    {
+
+        for(int j = 1; j <n*2; j++) 
+        {
+          int x = min(i,j);
+          int y = max(i,j);
+          int distance1 = abs(n - x) + 1;
+          int distance2 = abs(n - y) + 1;
+          cout<<max(distance1, distance2)<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+    
+}
+
+int main () 
+{
+    int n;
+    cin>>n;
+
+    hollowRectangle(n);
+    
+    return 0;
+}
+
